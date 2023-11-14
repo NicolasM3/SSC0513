1636a55a8ed86ca837c19adeb2e47f63fa7c8b9c
nicolas.oliveira <nicolas.oliveira@ifood.com.br>
2023-11-14 09:38:15 -0300

    feat: add otp and remove rsa

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@
     buffer: .space 100  
     newLine: .asciiz "\n"
     inputText: .asciiz "Digite um texto para criptografar: "
-    inputEncryption: .asciiz "(0) Cifra de cesar \n(1) OTP \n(2) RSA \nDigite o tipo de criptografia:"
+    inputEncryption: .asciiz "(0) Cifra de cesar \n(1) OTP \nDigite o tipo de criptografia:"
     inputProcess: .asciiz "Encriptar ou decriptar? (0) Encriptar (1) Decriptar:"
     
     inputCesarKey: .asciiz "(Cifra de cesar) Digite a chave: "
@@ -10,11 +10,6 @@
 
     inputOtpKey: .asciiz "(OTP) Digite a chave: "
     cesarOtpText: .asciiz "(OTP) CypherText: "
-
-    inputRsaPrivateKey1: .asciiz "(RSA) Digite a 1º chave privada: "
-    inputRsaPrivateKey2: .asciiz "(RSA) Digite a 2º chave privada: "
-    outputRsaPublicKey: .asciiz "(RSA) Chaves públicas: "
-    cesarRsaText: .asciiz "(RSA) CypherText: "
 
 .text
     main:
@@ -120,27 +115,9 @@
         j otpEncrypt
 
 
-    # RSA
+    # 3
     criptografia3:
-        # printa e lê a chave
-        li $v0, 4
-        la $a0, inputRsaPrivateKey1
-        syscall
-
-        li $v0,5
-        syscall
-        move $t1,$v0 
-
-        # printa e lê a chave
-        li $v0, 4
-        la $a0, inputRsaPrivateKey2
-        syscall
-
-        li $v0,5
-        syscall
-        move $t2,$v0 
-
-        j rsaEncrypt
+        
         
 
      # ------------------------------------ Cesar ------------------------------------
@@ -189,113 +166,6 @@
         move $a0, $t1
         syscall
         j otpEncrypt
-        
-    # ------------------------------------ RSA ------------------------------------
-
-    rsaEncrypt:
-        mult $t1, $t2
-        mflo $t3
-
-        li $v0, 4
-        la $a0, outputRsaPublicKey
-        syscall
-
-        # pula a linha
-        li $v0, 4
-        la $a0, newLine
-        syscall
-
-        # calcula o phi
-        sub $t1, $t1, 1
-        sub $t2, $t2, 1
-        mult $t1, $t2
-        mflo $t1
-        move $t2, $t3
-        # t1 - phi
-        # t2 - n
-
-        # printa o valor da chave 1º publica
-        li $v0, 1
-        move $a0, $t2
-        syscall
-
-        # pula a linha
-        li $v0, 4
-        la $a0, newLine
-        syscall
-
-        # valores para achar a 2º chave 
-        li $t3, 2
-        move $a0, $t1
-        move $a1, $t3
-
-        jal gcd
-
-        sub $t3, $t3, 1
-
-        # printa o valor da chave 2º publica
-        li $v0, 1
-        move $a0, $t3
-        syscall
-
-        # pula a linha
-        li $v0, 4
-        la $a0, newLine
-        syscall
-
-        # Chaver públicas calculadas
-        # t2 - n
-        # t3 - e
-
-        # encriptar letras
-        j encryptLetter
-                
-    gcd:
-        beq $a1, $zero, endGcd
-
-        div $a0, $a1
-        mfhi $t4  
-        move $a0, $a1
-        move $a1, $t4
-        j gcd
-
-    endGcd:
-        addi $t3, $t3, 1
-        move $t4, $a0
-
-        move $a0, $t1
-        move $a1, $t3
-
-        bne $t4, 1, gcd
-
-        jr $ra
-
-    encryptLetter:
-        lb $t1, 0($t0)
-        beq $t1,10,end 	 		# Termina o programa no \n 
-        beqz $t1,end  			# Termina o programa no final da string
-
-        move $a0, $t1
-        move $a1, $t3
-
-        addi $t0,$t0,1
-
-        jal power
-
-        div $t1, $t2
-        mfhi $t1
-
-    printRsaEncryptedChar:
-        li $v0, 1
-        move $a0, $t1
-        syscall
-
-        # pula a linha
-        li $v0, 4
-        la $a0, newLine
-        syscall
-
-        j encryptLetter
 
     
     # Metodo de pontenciação
@@ -306,6 +176,7 @@
         beqz $a1, endPower
         mult $a0, $t1
         mflo $t1
+
         addi $a1, $a1, -1
 
         j powerLoop
